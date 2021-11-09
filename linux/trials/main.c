9fb710f0020f0d08d10b000bbbd71c1b56038b25
Yuvraj Chauhan <seyuvraj290905@gmail.com>
2021-11-10 03:46:42 +0530

    did somethings with the authentication system but still stuck will think later today

--- linux/trials/main.c
+++ linux/trials/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <ctype.h>
 void delay(int number_of_seconds)
 {
     // Converting time into milli_seconds
@@ -16,18 +16,61 @@ void delay(int number_of_seconds)
 
 void welcome(void){
     printf("Welcome\n");
-    delay(10000);
+    delay(5000);
     system("clear");
 }
 
 char losi(void){
-    printf("(L)ogin\n(S)ignup");
+    system("clear");
+    printf("(L)ogin\n(S)ignup\n");
     char losi;
     scanf("%c",&losi);
     losi = tolower(losi);
     return losi;
 }
 int main(void){
+    //opening database
+    FILE *db = fopen("database.csv","a");
+    if(db == NULL){
+        printf("Some error opening file\n");
+        return 1;
+    }
+
+    //custom data-structure for profile
+    typedef struct
+    {
+        char username[20];
+        char password[16];
+    }
+    profile;
+    profile template_profile;
+
+    //Welcome Screen for 5 seconds
     welcome();
-    losi();
+    
+    //login and signup prompt
+    do
+    {
+        char a = losi();
+
+        if(a == 'l'){
+            printf("username:- ");
+            scanf("%s",template_profile.username);
+            printf("password:- ");
+            scanf("%s",template_profile.password);
+            break;
+        }
+        else if(a == 's'){
+            printf("username:- ");
+            scanf("%s",template_profile.username);
+            printf("password:- ");
+            scanf("%s",template_profile.password);
+            break;
+        }
+        else{
+            continue;
+        }
+    
+    }while(1);
+    
 }
